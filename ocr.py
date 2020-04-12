61f86e6799c20ad247112588b0d4730a22f86fe9
Michaela <mickeydbarcia@gmail.com>
2020-04-11 22:52:36 -0400

    lang

--- ocr.py
+++ ocr.py
@@ -5,15 +5,13 @@ import pyocr.builders
 
 tools = pyocr.get_available_tools()
 tool = tools[0]
-langs = tool.get_available_languages()
-lang = langs[0]
 
 def img_to_text(images):
     text_statement = ""
     for image in images:
         text_statement += tool.image_to_string(
             Image.open(image),
-            lang=lang,
+            lang='eng',
             builder=pyocr.builders.TextBuilder()
         )
     return text_statement
