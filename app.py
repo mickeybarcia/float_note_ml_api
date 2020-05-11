e3c546976911c90edc9055a225ed951bd9486a80
Michaela <mickeydbarcia@gmail.com>
2020-05-11 19:22:50 -0400

    try again

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ def handle_error(e):
 @app.route('/entry-image', methods = ['POST'])
 def generate_entry_ml_from_image():
 	check_auth(request.headers)
-	time.sleep(3)
 	images = request.files.getlist("page")
 	print(images)
+	time.sleep(3)
 	text = img_to_text(images)
 	if request.args.get('analyze') == "1":
 		print("analyzing images")
